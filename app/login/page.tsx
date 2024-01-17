95dba8c33f811f441d206e852630ce055172f29f
B3N_0'H4R4 <bwohara23+professional@gmail.com>
2024-01-17 16:37:55 +0000

    added supabase url + api key

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,7 +2,7 @@ import { createClient } from '@supabase/supabase-js'
 import { Auth } from '@supabase/auth-ui-react'
 import { ThemeSupa } from '@supabase/auth-ui-shared'
 
-const supabase = createClient('<INSERT PROJECT URL>', '<INSERT PROJECT ANON API KEY>')
+const supabase = createClient('https://xltnbmxlfecxgilzxpdl.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhsdG5ibXhsZmVjeGdpbHp4cGRsIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODk5NTQ3MzUsImV4cCI6MjAwNTUzMDczNX0.HLcSqhV99SW548RcJOlbJShcgIbOuD2jR6j_jTSCIpM')
 
 const App = () => (
   <Auth
