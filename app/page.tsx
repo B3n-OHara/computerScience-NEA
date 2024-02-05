8c8388a533ded3bdb5e43b1e9553118919ee3243
B3N_0'H4R4 <bwohara23+professional@gmail.com>
2024-02-05 10:57:26 +0000

    worked on homepage

--- app/page.tsx
+++ app/page.tsx
@@ -2,5 +2,35 @@ import "./globals.css";
 
 
 export default function Home() {
-    
+    return(
+        <div>
+            <div>
+                <h1>Containerise It.</h1>
+            </div>
+            
+            <div>
+                <h3>{"That's the general idea at least..."}</h3>
+            </div>
+            
+            <div>
+                <p>
+                    Need something to run on any machine, regardless of hardware?
+
+                    Need an easy way to manage CI/CD?
+
+                    Need a process to be secure and isolated?
+                </p>
+
+                <p>
+                    CONTAINERISE IT
+                </p>
+            </div>
+            
+            <div>
+                <p>
+                    So you've heard of containerisation. Or maybe you haven't 
+                </p>
+            </div>
+        </div>
+    )
 }
