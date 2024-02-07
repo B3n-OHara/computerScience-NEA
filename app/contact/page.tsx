ec51fe218ef1d7ef15e3f921b3e289f63e415cef
B3N_0'H4R4 <bwohara23+professional@gmail.com>
2024-02-07 20:55:12 +0000

    finished contact page structure

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,7 +1,27 @@
+import { SubmitContactForm } from "./actions"
+
 export default function contact() {
     return(
         <div>
-            
+            <form>
+                <div>
+                    <div>
+                        <label htmlFor="messageType">Message Type:</label>
+                        <select id="messageType" name="messageType">
+                            <option value={'Bug'}>Bug</option>
+                            <option value={'Feedback'}>Feedback</option>
+                            <option value={'Other'}>Other</option>
+                        </select>
+                    </div>
+                    <div>
+                        <label htmlFor="message">Message:</label>
+                        <input id="message" name="message" type="text" placeholder='Message'/>
+                    </div>
+                    <div>
+                        <button className="btn btn-wide" formAction={SubmitContactForm}>Submit</button>
+                    </div>
+                </div>
+            </form>
         </div>
     )
 }
