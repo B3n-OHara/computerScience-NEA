5c89762ec1adfec1696fce487a7325a5d3f8e2f5
B3N_0'H4R4 <bwohara23+professional@gmail.com>
2024-02-07 16:48:40 +0000

    sorted contact form route

--- app/contact/actions.ts
+++ app/contact/actions.ts
@@ -10,10 +10,20 @@ export async function SubmitContactForm(formData:FormData) {
     const cookieStore = cookies()
     const supabase = createClient(cookieStore)
 
-    const data = {
-        messageType: formData.get('messageType') as string,
-        message: formData.get('message') as string,
+    const messageType = formData.get('messageType') as string
+    const message = formData.get('message') as string
+
+    const { data: { user } } = await supabase.auth.getUser()
+
+    const { error } = await supabase
+        .from('ContactFormMessages')
+        .insert({ userID: (user), message_type: (messageType), message: (message) })
+
+    if (error) {
+        redirect('./error')
     }
 
-    //! need to send data off to supabase db + revalidate + redirect
+    revalidatePath('/', 'layout')
+    redirect('/')
+
 }
