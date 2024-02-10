738a8db3a15744273490869fcc0ff64f684ded64
B3N_0'H4R4 <bwohara23+professional@gmail.com>
2024-02-10 14:33:26 +0000

    styled login page

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -8,19 +8,19 @@ import googleIcon from '@/public/devicon--google.svg'
 export default function LoginPage() {
     return (
         <div>
-            <form className='flex flex-col justify-around absolute w-1/3 h-[700px] left-1/3 top-[140px] bg-01dp shadow-[0px_16px_6px_rgba(244,144,29,0.03)] shadow-[0px_9px_5px_rgba(244,144,29,0.1)] shadow-[0px_4px_4px_rgba(244,144,29,0.17)] shadow-[0px_1px_2px_rgba(244,144,29,0.2)] rounded-[20px]'>
-                <div className='flex flex-col gap-y-5'>
+            <form className='flex flex-col justify-evenly items-center absolute w-1/3 flex-initial h-[640px] left-1/3 top-[140px] bg-01dp shadow-[[0px_16px_6px_rgba(244,144,29,0.03)], [0px_9px_5px_rgba(244,144,29,0.1)], [0px_4px_4px_rgba(244,144,29,0.17)], [0px_1px_2px_rgba(244,144,29,0.2)]] rounded-[20px]'>
+                <div className='flex flex-col flex-initial gap-y-5 items-center'>
                     <div className='flex flex-col'>
                         <div className='label'>
                             <span className='label-text'>Email:</span>
                         </div>
-                        <input id="email" name="email" type="email" placeholder='name@email.com' className='input input-bordered input-lg left-[40px] top-[60px] w-[560px]'/>
+                        <input id="email" name="email" type="email" placeholder='name@email.com' className='input input-bordered input-lg left-[40px] top-[60px] flex-initial w-[520px]'/>
                     </div>
                     <div className='flex flex-col'>
                         <div className='label'>
                             <span className='label-text'>Passsword:</span>
                         </div>
-                        <input id="password" name="password" type="password" placeholder='Password'className='input input-bordered input-lg left-[40px] top-[60px] w-[560px]'/>
+                        <input id="password" name="password" type="password" placeholder='Password'className='input input-bordered input-lg left-[40px] top-[60px] flex-initial w-[520px]'/>
                     </div>
                 </div>
 
@@ -32,13 +32,14 @@ export default function LoginPage() {
                     <Link href="../(recoverAcc)">Forgotten Your Password?</Link>
                 </div>
 
-                <div className='flex flex-row'>
+                <div className='flex flex-initial gap-x-3'>
                     <div>
                         <button className='btn btn-lg' formAction={SignInWithGithub}>
                             Login With Github
                             <Image src={githubIcon} width={60} height={60} alt='Github Icon'/>
                         </button>
                     </div>
+                    
                     <div>
                         <button className='btn btn-lg' formAction={SignInWithGoogle}>
                             Login With Google
@@ -58,5 +59,3 @@ export default function LoginPage() {
         </div>
     )
 }
-
-//! NEED TO STYLE REST - REFER TO DAISY UI DOCS + TAILWIND DOCS
