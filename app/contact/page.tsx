0f905ad4bdd8d0c69aab56e6932facf0c27f1080
B3N_0'H4R4 <bwohara23+professional@gmail.com>
2024-02-14 19:49:48 +0000

    started styling contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -3,23 +3,26 @@ import { SubmitContactForm } from "./actions"
 export default function contact() {
     return(
         <div>
-            <form>
+            <form className='flex flex-col justify-around items-center absolute w-1/3 flex-wrap flex-initial h-[640px] left-1/3 top-[140px] bg-01dp shadow-[[0px_16px_6px_rgba(244,144,29,0.03)], [0px_9px_5px_rgba(244,144,29,0.1)], [0px_4px_4px_rgba(244,144,29,0.17)], [0px_1px_2px_rgba(244,144,29,0.2)]] rounded-[20px]'>
+                <div className="form-control w-5/6">
+                    <div className="label">
+                        <span className="label-text">Reason For Contact:</span>
+                    </div>
+                    <select id="contactReason" name="contactReason" className="select select-bordered">
+                        <option disabled selected>Select One</option>
+                        <option value={'Bug'}>Bug</option>
+                        <option value={'Feedback'}>Feedback</option>
+                        <option value={'Other'}>Other</option>
+                    </select>
+                </div>
+                <div className="form-control w-5/6">
+                    <div className="label">
+                        <span className="label-text">Message:</span>
+                    </div>
+                    <textarea id="message" name="message" placeholder='Message' className="textarea textarea-bordered textarea-lg "></textarea>
+                </div>
                 <div>
-                    <div>
-                        <label htmlFor="messageType">Message Type:</label>
-                        <select id="messageType" name="messageType">
-                            <option value={'Bug'}>Bug</option>
-                            <option value={'Feedback'}>Feedback</option>
-                            <option value={'Other'}>Other</option>
-                        </select>
-                    </div>
-                    <div>
-                        <label htmlFor="message">Message:</label>
-                        <input id="message" name="message" type="text" placeholder='Message'/>
-                    </div>
-                    <div>
-                        <button className="btn btn-wide" formAction={SubmitContactForm}>Submit</button>
-                    </div>
+                    <button className="btn btn-wide" formAction={SubmitContactForm}>Submit</button>
                 </div>
             </form>
         </div>
