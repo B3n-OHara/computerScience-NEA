c1df48cbddf10a8301d5339f538c0355378374f7
B3N_0'H4R4 <bwohara23+professional@gmail.com>
2024-02-20 18:06:29 +0000

    worked on styling of homepage

--- app/page.tsx
+++ app/page.tsx
@@ -25,10 +25,12 @@ export default function Home() {
                 </div>
             </div>
             
-            <div className="flex flex-col gap-y-24">
-                <div className="font-text italic text-8xl text-primary">
-                    <h1>Containerise It.</h1>
-                </div>
+            <div className="font-text italic text-8xl text-primary">
+                <h1>Containerise It.</h1>
+            </div>
+
+            <div className="grid grid-cols-3 gap-y-24">
+                
                 
                 <div>
                     <h3>{"That's the general idea at least..."}</h3>
@@ -53,27 +55,36 @@ export default function Home() {
                     <p>But you've scoured the internet looking for resources.</p>
                     <p>Your search results would have all pointed to lengthy Youtube videos, Docker, Kubernetes, Command Line Interfaces, orchestration, packages, dependencies, DockerFiles ......</p>
                     <p>To harness the power of containerisation you shouldn't have to piece everything together yourself via endless hours of scrolling through pages upon pages of various documentation.</p>
+                </div>
+                <div>
                     <p>That's where ContaineriseIt comes in</p>
                     <p>Clear and easy to use documnetation? Done</p>
                     <p>Detailed guides and walkthroughs? Done</p>
                     <p>Personal containers and virtual machines streamed to your browser to experiment with? Done</p>
+                </div>
+                <div>
                     <p>
                         The Community is a major part of software engineering, and it's guaranteed that if you have are stuck or have a question about something - someone else will too. 
                         That's why there's a <Link href={"./community"}>Community</Link> hub. 
                         Where you can host discussions, or post blog-like pages (written in markdown) to share your own thoughts on a topic, or to provide guides to others, or even to just share your own achievements.
                     </p>
+                </div>
+                <div>
                     <p>If that all sounds like something you can get behind,</p>
 
                     <Link href={"./auth/(signup)"}>Get Started</Link>  
-
-                    <p>
-                        ContaineriseIt is a project built for the AQA A Level Computer Science Non-Exam Assessment (NEA). 
-                        The NEA is designed to test not only student's coding skills, but also that of their documentation abilities.
-                        If you find any broken features or other bugs, please write a short summary of the issue in the contact page's form. <Link href={"./contact"}>Contact</Link>
-                    </p>
                 </div>
             </div>
             
+            <footer className="footer footer-center p-4 bg-base-300 text-base-content">
+                <aside>
+                    <p>
+                        ContaineriseIt is a project built for the AQA A Level Computer Science Non-Exam Assessment (NEA). 
+                        The NEA is designed to test not only student's coding skills, but also that of their documentation abilities.
+                        If you find any broken features or other bugs, please write a short summary of the issue in the contact page's form. <Link href={"./contact"} className="text-primary">Contact</Link>
+                    </p>
+                </aside>
+            </footer>
         </div>
     )
 }
