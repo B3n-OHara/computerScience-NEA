40ea266d637bc6630e9e48fe7cf646793514bd79
B3N_0'H4R4 <bwohara23+professional@gmail.com>
2024-02-21 18:02:14 +0000

    setup preferences form

--- app/dashboard/@loggedIn/account/renderAccountPreferences.tsx
+++ app/dashboard/@loggedIn/account/renderAccountPreferences.tsx
@@ -2,6 +2,7 @@
 
 import { createClient } from "@/utils/supabase/client"
 import React, { JSXElementConstructor, Key, PromiseLikeOfReactNode, ReactElement, ReactNode, ReactPortal, useEffect, useState } from "react"
+import { SubmitPreferences } from "./actions"
 
 export function RenderPreferences({
     children,
@@ -38,18 +39,97 @@ export function RenderPreferences({
     }, [supabase, children])
 
     return(
-        <form className="flex flex-col gap-y-8 m-8">
+        <form className="flex flex-col gap-y-8 m-8 font-text">
             <div>
                 {fetchError && (<p>{fetchError}</p>)}
                 {preferences && (
                     <div>
                         {preferences.map((preferences: { user_id: Key | null | undefined; level: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | PromiseLikeOfReactNode | null | undefined; interest1: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | PromiseLikeOfReactNode | null | undefined; interest2: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | PromiseLikeOfReactNode | null | undefined; interest3: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | PromiseLikeOfReactNode | null | undefined; use_case: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | PromiseLikeOfReactNode | null | undefined }) => (
-                            <div key={preferences.user_id}>
-                                <p>{preferences.level}</p>
-                                <p>{preferences.interest1}</p>
-                                <p>{preferences.interest2}</p>
-                                <p>{preferences.interest3}</p>
-                                <p>{preferences.use_case}</p>
+                            <div key={preferences.user_id} className="form-control w-1/3 gap-y-6">
+                                <div>
+                                    <div className="label">
+                                        <span className="label-text">Update Your Proficiency Level: </span>
+                                    </div>
+                                    <select id="level" name="level" className="select select-bordered">
+                                        <option disabled selected>Current Value: {preferences.level}</option>
+                                        <option value={'Beginner'}>Beginner</option>
+                                        <option value={'Intermediate'}>Intermediate</option>
+                                        <option value={'Expert'}>Expert</option>
+                                    </select>
+                                </div>
+                                <div>
+                                    <div className="label">
+                                        <span className="label-text">Update Your First Interest: </span>
+                                    </div>
+                                    <select id="interest1" name="interest1" className="select select-bordered">
+                                        <option disabled selected>Current Value: {preferences.interest1}</option>
+                                        <option value={'DevOps'}>DevOps</option>
+                                        <option value={'Deployment'}>Deployment</option>
+                                        <option value={'Microservices Architecture'}>Microservices Architecture</option>
+                                        <option value={'CI/CD'}>CI/CD</option>
+                                        <option value={'Infrastructure'}>Infrastructure</option>
+                                        <option value={'Scalability'}>Scalability</option>
+                                        <option value={'Efficiency'}>Efficiency</option>
+                                        <option value={'Portability'}>Portability</option>
+                                        <option value={'Security'}>Security</option>
+                                        <option value={'Testing'}>Testing</option>
+                                    </select>
+                                </div>
+                                <div>
+                                    <div className="label">
+                                        <span className="label-text">Update Your Second Interest: </span>
+                                    </div>
+                                    <select id="interest2" name="interest2" className="select select-bordered">
+                                        <option disabled selected>Current Value: {preferences.interest2}</option>
+                                        <option value={'DevOps'}>DevOps</option>
+                                        <option value={'Deployment'}>Deployment</option>
+                                        <option value={'Microservices Architecture'}>Microservices Architecture</option>
+                                        <option value={'CI/CD'}>CI/CD</option>
+                                        <option value={'Infrastructure'}>Infrastructure</option>
+                                        <option value={'Scalability'}>Scalability</option>
+                                        <option value={'Efficiency'}>Efficiency</option>
+                                        <option value={'Portability'}>Portability</option>
+                                        <option value={'Security'}>Security</option>
+                                        <option value={'Testing'}>Testing</option>
+                                    </select>
+                                </div>
+                                <div>
+                                    <div className="label">
+                                        <span className="label-text">Update Your Third Interest: </span>
+                                    </div>
+                                    <select id="interest3" name="interest3" className="select select-bordered">
+                                        <option disabled selected>Current Value: {preferences.interest3}</option>
+                                        <option value={'DevOps'}>DevOps</option>
+                                        <option value={'Deployment'}>Deployment</option>
+                                        <option value={'Microservices Architecture'}>Microservices Architecture</option>
+                                        <option value={'CI/CD'}>CI/CD</option>
+                                        <option value={'Infrastructure'}>Infrastructure</option>
+                                        <option value={'Scalability'}>Scalability</option>
+                                        <option value={'Efficiency'}>Efficiency</option>
+                                        <option value={'Portability'}>Portability</option>
+                                        <option value={'Security'}>Security</option>
+                                        <option value={'Testing'}>Testing</option>
+                                    </select>
+                                </div>
+                                <div>
+                                    <div className="label">
+                                        <span className="label-text">Update Your Primary Use Case: </span>
+                                    </div>
+                                    <select id="use_case" name="use_case" className="select select-bordered">
+                                        <option disabled selected>Current Value: {preferences.use_case}</option>
+                                        <option value={'DevOps'}>DevOps</option>
+                                        <option value={'Deployment'}>Deployment</option>
+                                        <option value={'Microservices Architecture'}>Microservices Architecture</option>
+                                        <option value={'CI/CD'}>CI/CD</option>
+                                        <option value={'Infrastructure'}>Infrastructure</option>
+                                        <option value={'Scalability'}>Scalability</option>
+                                        <option value={'Efficiency'}>Efficiency</option>
+                                        <option value={'Portability'}>Portability</option>
+                                        <option value={'Security'}>Security</option>
+                                        <option value={'Testing'}>Testing</option>
+                                    </select>
+                                </div>
+                                <button className="btn btn-lg btn-primary" formAction={SubmitPreferences}>Update Preferences</button>
                             </div>
                             
                         ))}
