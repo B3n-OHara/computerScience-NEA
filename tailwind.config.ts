fcc7e49d02e6a95d720cfa1741fcf3b32942722a
B3N_0'H4R4 <bwohara23+professional@gmail.com>
2024-02-24 13:56:00 +0000

    added tailwind typography plugin for prose class when rendering markdown

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -49,6 +49,9 @@ const config: Config = {
       },
     ],
   },
-  plugins: [require('daisyui')],
+  plugins: [
+    require('daisyui'),
+    require('@tailwindcss/typography')
+  ],
 }
 export default config
