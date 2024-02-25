a170a2fc949410da1191fd56f8316366da0689c8
B3N_0'H4R4 <bwohara23+professional@gmail.com>
2024-02-25 12:21:01 +0000

    reconfigured generateStaticParams

--- app/docs/[category]/[title]/page.tsx
+++ app/docs/[category]/[title]/page.tsx
@@ -24,7 +24,7 @@ export async function generateStaticParams() {
         }
 }
 
-export default function Page({
+export default async function Page({
     params,
 } : {
     params: { category: string; title: string }
