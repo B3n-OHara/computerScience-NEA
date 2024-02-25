700290ee54d55e455b4bdfef2723d0883bdd8b99
B3N_0'H4R4 <bwohara23+professional@gmail.com>
2024-02-25 09:44:19 +0000

    styling

--- app/docs/page.tsx
+++ app/docs/page.tsx
@@ -1,8 +1,9 @@
 export default function Docs() {
     return(
-        <div className="flex flex-col items-center">
-            <div className="prose">
+        <div className="flex flex-col items-center m-auto">
+            <div className="prose max-w-none w-3/5 text-center">
                 <h1>Welcome to the docs page</h1>
+                <p>Please Click On the Relevant Section In the Side Navigation To See Documentation</p>
             </div>
         </div>
     )
