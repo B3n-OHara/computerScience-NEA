3b8b00ca51f9645a1621cb43fc37aee78ee8776d
B3N_0'H4R4 <bwohara23+professional@gmail.com>
2024-02-26 22:00:23 +0000

    configured kasm

--- app/dashboard/@loggedIn/workspace/page.tsx
+++ app/dashboard/@loggedIn/workspace/page.tsx
@@ -1,7 +1,7 @@
 export default function Workspace() {
     return(
         <div className="flex flex-col items-center h-5/6 my-auto">
-            <iframe className="w-11/12 h-full" src="https://kasm.example.com" allow="autoplay; microphone; camera; clipboard-read; clipboard-write; window-management; self; https://kasm.example.com"></iframe>
+            <iframe className="w-11/12 h-full" src="https://kasm.containeriseit.dev" allow="autoplay; microphone; camera; clipboard-read; clipboard-write; window-management; self; https://kasm.containeriseit.dev"></iframe>
         </div>
     )
 }
