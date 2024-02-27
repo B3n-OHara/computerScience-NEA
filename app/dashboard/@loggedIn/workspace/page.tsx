4e5e0c5bc773a0738862a4d72cb2e9903d740eae
B3N_0'H4R4 <bwohara23+professional@gmail.com>
2024-02-27 20:15:21 +0000

    added default kasm pwd prompt

--- app/dashboard/@loggedIn/workspace/page.tsx
+++ app/dashboard/@loggedIn/workspace/page.tsx
@@ -2,6 +2,7 @@ export default function Workspace() {
     return(
         <div className="flex flex-col items-center h-5/6 my-auto">
             <iframe className="w-11/12 h-full" src="https://kasm.containeriseit.dev" allow="autoplay; microphone; camera; clipboard-read; clipboard-write; window-management; self; https://kasm.containeriseit.dev"></iframe>
+            <p>Default Password For First Time Users: newKasmUser123!</p>
         </div>
     )
 }
