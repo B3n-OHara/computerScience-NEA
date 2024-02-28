f51e403782d56ecf81e5c604f38ee6affba57d3f
B3N_0'H4R4 <bwohara23+professional@gmail.com>
2024-02-28 16:27:18 +0000

    commenting

--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -1,17 +1,19 @@
 import { createServerClient, type CookieOptions } from '@supabase/ssr'
 import { NextResponse, type NextRequest } from 'next/server'
 
+//updates website session based on current route, ensures users are persistent between redirects & refreshes
 export async function updateSession(request: NextRequest) {
   let response = NextResponse.next({
     request: {
       headers: request.headers,
     },
   })
-
+  //creates supabase server client
   const supabase = createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
+      //carries out methods on cookies
       cookies: {
         get(name: string) {
           return request.cookies.get(name)?.value
@@ -53,7 +55,8 @@ export async function updateSession(request: NextRequest) {
       },
     }
   )
-
+  
+  //gets current user from supabase
   await supabase.auth.getUser()
 
   return response
--- utils/supabase/server.ts
+++ utils/supabase/server.ts
@@ -3,11 +3,15 @@ import { cookies } from 'next/headers'
 
 export function createClient() {
   const cookieStore = cookies()
-
+  
+  //creates a client stored on both the browser and server using cookies
   return createServerClient(
+    //processes NEXT_PUBLIC_SUPABASE_URL (the URL of the supabase project & stored on the server for protection) securely and uses as an argument
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    //processes NEXT_PUBLIC_SUPABASE_ANON_KEY (the Anonymous API Key for supabase & stored on the server for protection) securely and uses as an argument
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
+      //provides different methods to use on cookies object
       cookies: {
         get(name: string) {
           return cookieStore.get(name)?.value
